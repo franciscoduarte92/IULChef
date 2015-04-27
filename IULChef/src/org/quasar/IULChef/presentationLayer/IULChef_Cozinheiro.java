46afdd8f1fdc47d12aa04c056f364880c065f185
franciscoduarte92 <francisco_duarte_92@hotmail.com>
2015-04-27 07:31:50 +0100

    .

--- IULChef/src/org/quasar/IULChef/presentationLayer/IULChef_Cozinheiro.java
+++ IULChef/src/org/quasar/IULChef/presentationLayer/IULChef_Cozinheiro.java
@@ -27,14 +27,19 @@ public class IULChef_Cozinheiro extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+    	
+    	addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e){
+				Database.close();
+				new IULChef_Login();
+            }
+		});
 
         jPanel1 = new javax.swing.JPanel();
         jButtonEliminarDevolucoes = new javax.swing.JButton();
         jButtonInventario = new javax.swing.JButton();
         jButtonNovoProdutoComposto = new javax.swing.JButton();
         jButtonVoltar = new javax.swing.JButton();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButtonEliminarDevolucoes.setText("Eliminar Devoluções");
         jButtonEliminarDevolucoes.addActionListener(new java.awt.event.ActionListener() {
@@ -107,6 +112,7 @@ public class IULChef_Cozinheiro extends javax.swing.JFrame {
         );
 
         pack();
+        setVisible(true);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonEliminarDevolucoesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEliminarDevolucoesActionPerformed
@@ -124,41 +130,6 @@ public class IULChef_Cozinheiro extends javax.swing.JFrame {
     private void jButtonVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonVoltarActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButtonVoltarActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(IULChef_Cozinheiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(IULChef_Cozinheiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(IULChef_Cozinheiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(IULChef_Cozinheiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new IULChef_Cozinheiro().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonEliminarDevolucoes;
