c1f939ffb9f3129ae4e6627b8e3fa6f69a63ab2e
franciscoduarte92 <francisco_duarte_92@hotmail.com>
2015-04-27 09:01:08 +0100

    .

--- IULChef/src/org/quasar/IULChef/presentationLayer/Main_IULChef_GUI.java
+++ IULChef/src/org/quasar/IULChef/presentationLayer/Main_IULChef_GUI.java
@@ -5,7 +5,7 @@ import org.quasar.IULChef.persistenceLayer.Database;
 public class Main_IULChef_GUI {
 
 	public static void main(String[] args) {
-			Database.open("/Users/terroma/git/IULChef/IULChef/database", "IULChef", "db4o");
+			Database.open("C:/Users/Francisco/Documents/GitHub/IULChef/IULChef/database", "IULChef", "db4o");
 			
 			new IULChef_Login();
 	}
